b210c421711c5bf29a83ff8e8fd2eb126359270e
bradengc <bradenescott@hotmail.ca>
2020-10-22 01:17:56 -0400

    update

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,24 @@
+let votesArray = [0, 0, 0, 0];
+
 exports.handler = async (event) => {
-  const subject = event.queryStringParameters.name || "World";
+  const v = event.queryStringParameters.name;
+
+  if (v === "a") {
+    votesArray[0] += 1;
+  } else if (v === "b") {
+    votesArray[0] += 1;
+  } else if (v === "c") {
+    votesArray[0] += 1;
+  } else if (v === "d") {
+    votesArray[0] += 1;
+  } else if (v === "r") {
+    votesArray = [0, 0, 0, 0];
+  }
+  console.log(JSON.stringify(votesArray));
+
   return {
     statusCode: 200,
+    header: { "Access-Control-Allow-Origin": '"' },
     body: `Hello ${subject}!`,
   };
 };
